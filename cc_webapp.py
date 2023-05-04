f2296b8b7ddb75e9a1dacea0e03281a8959bf77b
SusroonyaKoduru <98198329+SusroonyaKoduru@users.noreply.github.com>
2023-05-03 22:16:27 -0500

    Update cc_webapp.py

--- cc_webapp.py
+++ cc_webapp.py
@@ -1,84 +1,55 @@
-from __future__ import division, print_function
-# coding=utf-8
-import sys
-import os
-import glob
-import re
-import numpy as np
-
-# Keras
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from keras.models import load_model
-from keras.preprocessing import image
-
-# Flask utils
-from flask import Flask, redirect, url_for, request, render_template
+import os 
+from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
 
-# Define a flask app
+
 app = Flask(__name__)
 
-# Model saved with Keras model.save()
-MODEL_PATH = 'C:/Users/susro/OneDrive/Desktop/CloudComputing/model.h5'
+from keras.models import load_model 
+from keras.backend import set_session
+from skimage.transform import resize 
+import matplotlib.pyplot as plt 
+import tensorflow as tf 
+import numpy as np 
 
-# Load your trained model
-model = load_model(MODEL_PATH)
-model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
+print("Loading model") 
+global sess
+sess = tf.Session()
+set_session(sess)
+global model 
+model = load_model('model.h5') 
+global graph
+graph = tf.get_default_graph()
 
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('')
-print('Model loaded. Check http://127.0.0.1:5000/')
-
-
-def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(224, 224))
-
-    # Preprocessing the image
-    x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='caffe')
-
-    preds = model.predict(x)
-    return preds
-
-
-@app.route('/', methods=['GET'])
-def index():
-    # Main page
+@app.route('/', methods=['GET', 'POST']) 
+def main_page():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join('uploads', filename))
+        return redirect(url_for('prediction', filename=filename))
     return render_template('index.html')
 
+@app.route('/prediction/<filename>') 
+def prediction(filename):
+    #Step 1
+    my_image = plt.imread(os.path.join('uploads', filename))
+    #Step 2
+    my_image_re = resize(my_image, (48,48,3))
+    
+    #Step 3
+    with graph.as_default():
+      set_session(sess)
+      probabilities = model.predict(np.array( [my_image_re,] ))[0,:]
+      print(probabilities)
+      #Step 4
+      number_to_class = ['anger','contempt','disgust','fear','happy','sadness','surprise']
+      index = np.argsort(probabilities)
+      predictions = {
+        "class1":number_to_class[index[6]],
+		"class2":number_to_class[index[5]],
+      }
+    #Step 5
+    return render_template('predict.html', predictions=predictions)
 
-@app.route('/predict', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        # Get the file from post request
-        f = request.files['file']
-
-        # Save the file to ./uploads
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
-
-        # Make prediction
-        preds = model_predict(file_path, model)
-
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
-        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-        result = str(pred_class[0][0][1])               # Convert to string
-        return result
-    return None
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(host='0.0.0.0', port=80)
