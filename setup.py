5cde86f8e948a20b2e5de49d9756f9bc06b06175
SusroonyaKoduru <98198329+SusroonyaKoduru@users.noreply.github.com>
2023-05-04 14:11:43 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ccproject",
+    version="0.0.1",
+    author="SusroonyaKoduru",
+    author_email="kodurususroonya@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
